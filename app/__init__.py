f51878f8fade4a443fe0819a74b317faf012191b
Niklas Meinzer <meinzer.niklas@gmail.com>
2014-09-14 17:45:27 +0200

    Add email support

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,35 @@
 from flask import Flask
 from flask.ext.babel import Babel
+from flask.ext.mail import Mail
+import app.mail_config as mailconfig
 
 
 class ConfigClass(object):
     # Configure Flask
     SECRET_KEY = 'THIS IS AN INSECURE SECRET'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///minimal_app.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///fobiwe.sqlite'
     CSRF_ENABLED = True
-    USER_ENABLE_EMAIL = False
+    USER_ENABLE_USERNAME = False
+    USER_ENABLE_CHANGE_USERNAME = False
+    USER_ENABLE_EMAIL = True
     DEFAULT_LOCALE = "de"
+    # Configure Flask-User
+    USER_ENABLE_CONFIRM_EMAIL    = True
+    USER_ENABLE_CHANGE_PASSWORD  = True
+    USER_ENABLE_FORGOT_PASSWORD  = True
+    USER_ENABLE_RETYPE_PASSWORD  = True
+
+    # Configure Flask-Mail -- Required for Confirm email and Forgot password features
+    MAIL_SERVER   = mailconfig.MAIL_SERVER
+    MAIL_PORT     = 465
+    MAIL_USE_SSL  = True # Some servers use MAIL_USE_TLS=True instead
+    MAIL_USERNAME = mailconfig.MAIL_ADDRESS
+    MAIL_PASSWORD = mailconfig.MAIL_PASSWORD
+    MAIL_DEFAULT_SENDER = mailconfig.MAIL_ADDRESS
 
 app = Flask(__name__)
 app.config.from_object(__name__ + '.ConfigClass')
 babel = Babel(app)      # Initialize Flask-Babel
+mail = Mail(app)
 
 from app import views  # noqa
--- app/db.py
+++ app/db.py
@@ -48,12 +48,15 @@ def setup_db():
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     active = db.Column(db.Boolean(), nullable=False, default=False)
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False, default='')
+    email = db.Column(db.Text, nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
     room_id = db.Column(db.Integer, db.ForeignKey("room.id"))
     qualification = db.Column(db.Enum(*QUALIFICATIONS), nullable=False)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
+    confirmed_at = db.Column(db.DateTime())
+    reset_password_token = db.Column(db.String(100), nullable=False, default='')
+
 
 
 class Room(db.Model):
